2377c5b96df9bdc3270859dab33cccae47063a8a
mavstronaut <mavbarona@gmail.com>
2019-08-27 13:50:02 -0400

    creating Nav components

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+  return (
+    <nav className="nav navbar-expand-lg navbar-dark">
+      <a className="nav-link text-info" href="/">
+        <h3>Google Books</h3>
+      </a>
+      <a className="nav-link mt-2 text-info" href="/">Audio</a>
+      <a className="nav-link mt-2 text-info" href="/login">Login</a>
+      <a className="nav-link mt-2 text-info" href="/video">Video: coming Soon</a>
+    </nav>
+  );
+}
+
+export default Nav;
