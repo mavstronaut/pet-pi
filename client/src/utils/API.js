7b37cdae680c2a3d94fc5c992bd4fd4d817d955f
mavstronaut <mavbarona@gmail.com>
2019-08-29 18:53:18 -0400

    adding search function to client api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,24 @@
 import axios from "axios";
+import youtube from "simple-youtube-api";
 
 export default {
   // call to the Google Book API when searching for a book
   searchSounds: function(query) {
-    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query);
+    let id = (() => {
+      const parsed = parse(song, true);
+      if (/^(www\.)?youtube\.com/.test(parsed.hostname)) {
+        return parsed.query.v;
+      } else if (/^(www\.)?youtu\.be/.test(parsed.hostname)) {
+        return parsed.pathname.slice(1);
+      }
+    })();
+  
+    if (!id) {
+      let results = await youtube.searchVideos(song, 4);
+      id = results[0].id;
+    }
+
+    return id;
   },
 
   // display the saved sounds from our database
