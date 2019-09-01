9481c1f6803af10c138fe68170b81b1da1e3c274
mavstronaut <mavbarona@gmail.com>
2019-08-31 22:13:23 -0400

    creating user route for api>controller

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 const soundRoutes = require("./sounds");
 
+// adding login es6 syntax
+import { userRouter } from './api/user';
+
+
 // Sound routes
 router.use("/sounds", soundRoutes);
 
 // User routes
-router.use("/login", jwt);
-router.use("/user");
+// router.use("/login", jwt);
+router.use("/user", userRouter);
 
 module.exports = router;
---
+++ routes/api/user.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const userController = require("../../controllers/userController");
+
+// Signup
+router.route("/signup")
+	.post(userController.signup);
+
+// Login
+router.route("/login")
+  	.get(userController.login);
+
+// authenticate
+router.route("/authenticate")
+    .get(passport.authenticate('jwt', {session: false}), userController.authenticate);
+	  
+	  
+// google auth
+ router.route("/auth/google")
+	.get(passport.authenticate('google', { scope: ['profile', 'email']}));
+	
+// google auth fails
+router.route("/auth/google/callback")
+	.get(passport.authenticate('google', { failureRedirecdt: '/login' }),
+    userController.authSucccess);
+
+module.exports = router;
