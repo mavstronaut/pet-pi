8fd17ba7a3840ce538ac7df0ca18112a2655af9b
mavstronaut <mavbarona@gmail.com>
2019-09-09 09:46:25 -0400

    giving hard coded results an id

--- routes/api/hardSounds.js
+++ routes/api/hardSounds.js
@@ -1,71 +1,83 @@
 var audioDefault = [
     {
+        id: 1,
         name: 'dog-whistle-low-high',
         type: 'distract',
         link: 'https://www.youtube.com/embed/qMwUZ6j3PV8',
         thumbnail: 'https://i.imgur.com/Ybt2FB6.png'
     },
     {
+        id: 2,
         name: 'birds-chirping',
         type: 'good',
         link: 'https://www.youtube.com/embed/Qm846KdZN_c',
         thumbnail: 'https://i.imgur.com/RS87ned.png'
     },
     {
+        id: 3,
         name: 'dog-whistle',
         type: 'alert',
         link: 'https://www.youtube.com/embed/gfxewnndNQ8',
         thumbnail: 'https://i.imgur.com/Ybt2FB6.png'
     },
     {
+        id: 4,
         name: 'mosquito-tone',
         type: 'alert',
         link: 'https://www.youtube.com/embed/GyNcpW8o-i4',
         thumbnail: 'https://i.imgur.com/jFSgep0.png'
     },
     {
+        id: 5,
         name: 'trevor-hall',
         type: 'music',
         link: 'https://www.youtube.com/embed/XwHVS7Q5cOI',
         thumbnail: 'https://i.imgur.com/nES9eOx.png'
     },
     {
+        id: 6,
         name: 'The-Adventure-Zone',
         type: 'good',
         link: 'https://www.youtube.com/embed/HfzGrlNXuhk',
         thumbnail: 'https://i.imgur.com/RS87ned.png'
     },
     {
+        id: 7,
         name: 'audience-cheer',
         type: 'distract',
         link: 'https://www.youtube.com/embed/tq-Bp_2FOGA',
         thumbnail: 'https://i.imgur.com/nES9eOx.png'
     },
     {
+        id: 8,
         name: 'air-horn',
         type: 'distract',
         link: 'https://www.youtube.com/embed/OFr74zI1LBM',
         thumbnail: 'https://i.imgur.com/nES9eOx.png'
     },
     {
+        id: 9,
         name: 'fog-horn',
         type: 'distract',
         link: 'https://www.youtube.com/embed/8dENYJbN1z4',
         thumbnail: 'https://i.imgur.com/R3q09Me.png'
     },
     {
+        id: 10,
         name: 'whistle',
         type: 'distract',
         link: 'https://www.youtube.com/embed/9UBxlyCk8RE',
         thumbnail: 'https://i.imgur.com/R3q09Me.png'
     },
     {
+        id: 11,
         name: 'hmm-all',
         type: 'distract',
         link: 'https://www.youtube.com/embed/hNEmxbdKx9k',
         thumbnail: 'https://i.imgur.com/R3q09Me.png'
     },
     {
+        id: 12,
         name: 'beeps',
         type: 'alert',
         link: 'https://www.youtube.com/embed/Dq196N-mHSE',
